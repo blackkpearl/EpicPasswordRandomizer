0da60012bd46794ce7f16b349c25da83515495da
Black Pearl <arraa.marie@gmail.com>
2020-10-07 19:14:45 -0500

    Fixed repetitive truetruetrue error in syntax.

--- script.js
+++ script.js
@@ -5,68 +5,73 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var specialChar = "!@#$%^&*()-_=+[{}]|\;:'<>?/,.".split("");
 var numbers = "1234567890".split("");
-var newArray =[];
+var newArray = [];
 
 console.log(upperCase);
 console.log(lowerCase);
 console.log(specialChar);
 console.log(numbers);
 
+
+
 // Write password to the #password input
 function generatePassword() {
-var CharNum = prompt("Please select a number between 8 and 128 for the character length of your password.");
-CharNum = parseInt(CharNum);
-  if (CharNum >=8 && CharNum <=128); {
+  var CharNum = prompt("Please select a number between 8 and 128 for the character length of your password.");
+  CharNum = parseInt(CharNum);
+  if (CharNum >= 8 && CharNum <= 128) {
     console.log(CharNum)
-  } 
-  
-  
-// Confirms the user would like uppercase letters included in password. 
-var upperCase = confirm("Would you like to include uppercase letters?");
-  if(upperCase){
-newArray = newArray.concat(upperCase);
-console.log(newArray);
-}
+  } else {
+    alert("You did not select a number between 8 and 128. Try again.");
+    return;
+  }
 
-// Confirms the user would like lowercase letters included in password. 
-var lowerCase = confirm("Would you like to include lowercase letter?");
-  if(lowerCase){
-newArray = newArray.concat(lowerCase)
-console.log(newArray);
-}
-
-// Confirms the user would like special characters included in password. 
-var specialChar = confirm("Would you like to include special characters?");
-  if(specialChar){
-  newArray = newArray.concat(specialChar)
+  // Confirms the user would like uppercase letters included in password. 
+  var upperCaseletters = confirm("Would you like to include uppercase letters?");
+  if (upperCaseletters) {
+    newArray = newArray.concat(upperCase);
+  }
   console.log(newArray);
-}
 
-// Confirms the user would like numbers included in password.
-var numbers = confirm("Would you like to include numbers?");
-  if(numbers){
-newArray = newArray.concat(numbers)
-console.log(newArray);
-}
+  // Confirms the user would like lowercase letters included in password. 
+  var lowerCaseletters = confirm("Would you like to include lowercase letter?");
+  if (lowerCaseletters) {
+    newArray = newArray.concat(lowerCase)
+    console.log(newArray);
+  }
 
-// If the user did not make any selections.
-if (!lowerCase && !upperCase && !specialChar && !numbers){
-  alert("You did not make a selection. Please try again.")
-  return;  
-}
+  // Confirms the user would like special characters included in password. 
+  var specialCharletters = confirm("Would you like to include special characters?");
+  if (specialCharletters) {
+    newArray = newArray.concat(specialChar)
+    console.log(newArray);
+  }
 
-var password = []
-for (i = 0; i < CharNum; i++) {
-  password.push(newArray[Math.floor(Math.random() *newArray.length)]);
- }
- return password.join("");
-}
+  // Confirms the user would like numbers included in password.
+  var number = confirm("Would you like to include numbers?");
+  if (number) {
+    newArray = newArray.concat(numbers)
+    console.log(newArray);
+  }
 
-function writePassword(){
+  // If the user did not make any selections.
+  if (!lowerCase && !upperCase && !specialChar && !numbers) {
+    alert("You did not make a selection. Please try again.")
+    return;
+  }
+
+  console.log(newArray);
+
+  var password = []
+  for (i = 0; i < CharNum; i++) {
+    password.push(newArray[Math.floor(Math.random() * newArray.length)]);
+  }
+  return password.join("");
+}
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
